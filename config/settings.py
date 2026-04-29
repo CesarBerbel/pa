6477dc7afcc1c6078e521391d67aadc2aec5486a
César Berbel <cesarberbelqa@gmail.com>
2026-04-29 01:12:14 +0100

    settings changes

--- config/settings.py
+++ config/settings.py
@@ -93,7 +93,11 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
